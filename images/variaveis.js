9c902a0d507e28b4f36a774763c8822f623974fa
Atanes - Trybe <alexandre.atanes@betrybe.com>
2022-03-11 12:26:19 -0300

    Arquivo com exemplos de declaraçao de variaveis e mudanças de tipo dados dinamica

---
+++ images/variaveis.js
@@ -0,0 +1,33 @@
+// Valores number
+let numero = 13;
+let moeda = 45.50;
+// Valor string
+let classe = 'especial';
+// Um array
+let animais = ['gatos', 'cachorros', 'peixes', 'aves'];
+// Um objeto
+let pessoa = {
+  nome: 'Alexandre',
+  idade: 51,
+  profissao: 'professor'
+};
+// Valor boleano
+let maiorIdade = true;
+// Valor nulo;
+let concluido = null;
+// Se um valor não for atribuido a variável recebe um valor indefinido (undefined)
+let data;
+
+console.log('Valores numéricos:', numero, 'e', moeda);
+console.log('Valor string:', classe);
+console.log('Um array:', animais);
+console.log('Um objeto:', pessoa);
+console.log('Valor boleano:', maiorIdade, '\nValor nulo', concluido);
+console.log('Valor indefinido:', data);
+
+// Atribuindo novos valores para as variáveis e trocando alguns 'tipos de dados' de forma dinamica!
+
+numero = 5.5;
+moeda = 100;
+classe = 'Executiva';
+maiorIdade = 'sim';
